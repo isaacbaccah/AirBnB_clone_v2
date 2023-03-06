e938363fae0d83d72c92c61f33ef4de43b5bddc5
Isaac Baccah <isaacbaccah@icloud.com>
2023-03-06 13:48:13 -0800

    new commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Importing the needed framework"""
+"""
+starts a Flask web application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Function that returns a simple string"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """Function that returns HBNB strint"""
-    return "HBNB"
-
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Importing the needed framework
-A script that starts a Flask web application
 """
+starts a Flask web application
+"""
+
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Function that returns a simple string"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_hbnb():
-    """Function that returns HBNB string"""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c_print(text):
-    """Function that returns a C string"""
-    result = text.replace('_', ' ')
-    return "C {}".format(result)
-
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
